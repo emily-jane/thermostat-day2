ccf00b8561628f538415d535ef44dd4edcb54546
Emily Carson <emilyjanecarson@gmail.com>
2015-08-19 17:22:16 +0100

    added more jquery testing

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -8,7 +8,44 @@ describe('Thermostat',function() {
   });
 
   it('displays default temperature', function(){
-    expect('span').toContainText('20');
+    expect('#temperature').toContainText(20);
+  });
+
+  it('up button should increase temperature', function() {
+    $('#up').click();
+    expect('#temperature').toContainText(21);
+  });
+
+  it('down button should decrease temperature', function() {
+    $('#down').click();
+    expect('#temperature').toContainText(19);
+  });
+
+  it('should have a minimum temperature of 10', function() {
+    for(i = 1; i < 15; i ++) {
+      $('#down').click();
+    };
+    expect('#temperature').toContainText(10);
+  });
+
+  it('should have power saver on by default', function() {
+    expect($('input[type=checkbox]')).toBeChecked();
+  });
+
+  it('should have a max temperature of 25 if power saver is on', function() {
+    for(i = 1; i < 10; i ++) {
+      $('#up').click();
+    };
+    expect('#temperature').toContainText(25);
+  });
+
+  it('should have a max temperature of 32 if power saver is off', function() {
+    $('input[type=checkbox]').click();
+    console.log($('input[type=checkbox]').prop('checked'));
+    for(i = 1; i < 20; i ++) {
+      $('#up').click();
+    };
+    expect('#temperature').toContainText(32);
   });
 
 });
--- src/app_jquery.js
+++ src/app_jquery.js
@@ -1,4 +1,21 @@
 $( document ).ready(function() {
 
+var thermostat = new Thermostat();
+
+var updateTemperature = function() {
+  $('#temperature').html(thermostat.temperature);
+};
+
+updateTemperature();
+
+$('#up').click(function() {
+  thermostat.up();
+  updateTemperature();
+});
+
+$('#down').click(function() {
+  thermostat.down();
+  updateTemperature();
+});
 
 });
